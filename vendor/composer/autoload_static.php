d06863c1483c252a82f6b2d9cfde49ee403392a2
Vinicius Souza <vinicius.sfti@gmail.com>
2017-09-05 17:30:14 -0300

    I got the database connection and added autload class

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit113683b9dc74b6b90d5cee722b9c0e09
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit113683b9dc74b6b90d5cee722b9c0e09
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit113683b9dc74b6b90d5cee722b9c0e09::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit113683b9dc74b6b90d5cee722b9c0e09::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit113683b9dc74b6b90d5cee722b9c0e09::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit113683b9dc74b6b90d5cee722b9c0e09::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit113683b9dc74b6b90d5cee722b9c0e09::$classMap;
 
