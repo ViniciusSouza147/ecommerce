6c600459a54e4e095c714909c8c6e7b6be7c3b60
Vinicius Souza <vinicius.sfti@gmail.com>
2017-09-11 17:11:04 -0300

    Error URL

--- index.php
+++ index.php
@@ -5,30 +5,55 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+
 
 $app = new Slim();
 
 $app->config('debug', true);
 /*==============================*/
-// Start Route site Ecommerce
+// Starts the ecommerce site route.
 $app->get('/', function() {
     
     $page = new Page();
     $page->setTpl("index");
 
 });
-//End Route site Ecommerce 
+//End the Ecommerce site route. 
 
 /*==============================*/
 
-//Start Route site Admin
+//Starts admin site route.
 $app->get('/admin', function() {
     
     $page = new PageAdmin();
     $page->setTpl("index");
 
 });
-//End Route site Admin 
+//Ends the admin site route. 
+
+/*==============================*/
+
+//Starts the login page routes.
+$app->get('/admin/login', function() {
+    
+    $page = new PageAdmin([
+    	"header"=>false,
+    	"footer"=>false
+    ]);
+    $page->setTpl("login");
+    exit;
+});
+
+//validate login and password.
+$app->get('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+	header("Locarion: /admin");
+	exit;
+
+});
+//Ends the login page routes. 
 /*==============================*/
 
 $app->run();
