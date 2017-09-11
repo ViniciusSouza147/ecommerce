3e57bd8087275c5ad611b95da8877e9649fdca05
Vinicius Souza <vinicius.sfti@gmail.com>
2017-09-11 10:52:15 -0300

    Fatal Erro Line on line 3 in the file PageAdmin.php

--- index.php
+++ index.php
@@ -4,17 +4,32 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
-
+/*==============================*/
+// Start Route site Ecommerce
 $app->get('/', function() {
     
     $page = new Page();
     $page->setTpl("index");
 
 });
+//End Route site Ecommerce 
+
+/*==============================*/
+
+//Start Route site Admin
+$app->get('/admin', function() {
+    
+    $page = new PageAdmin();
+    $page->setTpl("index");
+
+});
+//End Route site Admin 
+/*==============================*/
 
 $app->run();
 
