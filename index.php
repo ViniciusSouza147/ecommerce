8b2b9112205f0579eb7d23d18c64dec5103ef419
Vinicius Souza <vinicius.sfti@gmail.com>
2017-09-15 16:27:18 -0300

    Error 2017/09/15.

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ $app = new Slim();
 $app->config('debug', true);
 /*==============================*/
 // Starts the ecommerce site route.
-$app->get('/', function() {
+$app->get('/', function()
+{
     
     $page = new Page();
     $page->setTpl("index");
@@ -24,7 +25,8 @@ $app->get('/', function() {
 /*==============================*/
 
 //Starts admin site route.
-$app->get('/admin', function() {
+$app->get('/admin', function() 
+{
     
     User::verifyLogin();
     $page = new PageAdmin();
@@ -36,7 +38,8 @@ $app->get('/admin', function() {
 /*==============================*/
 
 //Starts the login page routes.
-$app->get('/admin/login', function() {
+$app->get('/admin/login', function()
+{
     
     $page = new PageAdmin([
     	"header"=>false,
@@ -48,7 +51,8 @@ $app->get('/admin/login', function() {
 
 //Method Post - Validate login and password.
 
-$app->post('/admin/login', function() {
+$app->post('/admin/login', function()
+{
 
 	User::login($_POST["login"], $_POST["password"]);
 	header("Location: /admin");
@@ -58,7 +62,8 @@ $app->post('/admin/login', function() {
 //Ends the login page routes. 
 
 //Logout the admin site route.
-$app->get('/admin/logout', function(){
+$app->get('/admin/logout', function()
+{
 
 	User::logout();
 	header("Location: /admin/login");
@@ -69,7 +74,8 @@ $app->get('/admin/logout', function(){
 
 // =========== Starts user CRUD rotes ============= //
 //Start the route user page.
-$app->get("/admin/users", function(){
+$app->get("/admin/users", function()
+{
 
 	User::verifyLogin();
 	$users = User::listAll();
@@ -81,7 +87,8 @@ $app->get("/admin/users", function(){
 });
 
 // Criate User.
-$app->get("/admin/users/create", function(){
+$app->get("/admin/users/create", function()
+{
 
 	User::verifyLogin();
 	$page = new PageAdmin();
@@ -90,27 +97,93 @@ $app->get("/admin/users/create", function(){
 });
 
 // Delete User
-$app->get("/admin/users/:iduser/delete", function($iduser){
+$app->get("/admin/users/:iduser/delete", function($iduser)
+{
+	
 	User::verifyLogin();
+	$user = new User();
+	$user->get((int)$iduser);
+	$user->delete();
+	header("Location: /admin/users");
+	exit;
+
 });
 
 //Update User.
-$app->get("/admin/users/:iduser", function($iduser){
+$app->get("/admin/users/:iduser", function($iduser)
+{
 
 	User::verifyLogin();
+	$user = new User();
+	$user->get((int)$iduser);
 	$page = new PageAdmin();
-	$page->setTpl("users-update");
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()		
+	));
 
 });
 
 // Insert User
-$app->post("/admin/users/create", function(){
+$app->post("/admin/users/create", function()
+{
+
 	User::verifyLogin();
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$user->setData($_POST);
+	$user->save();
+	header("Location: /admin/users");
+	exit;
+
 });
 
 // To save User
-$app->post("/admin/users/:iduser", function($iduser){
+$app->post("/admin/users/:iduser", function($iduser)
+{
+	
 	User::verifyLogin();
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$user->get((int)$iduser);
+	$user->setData($_POST);
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get("/admin/forgot", function()
+{
+
+	$page = new PageAdmin([
+    	"header"=>false,
+    	"footer"=>false
+    ]);
+    $page->setTpl("forgot");
+    exit;
+
+});
+
+$app->post("/admin/forgot", function()
+{
+
+	$user = User::getForgot($_POST["email"]);
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+$app->get("/admin/forgot/sent", function()
+{
+
+	$page = new PageAdmin([
+    	"header"=>false,
+    	"footer"=>false
+    ]);
+    $page->setTpl("forgot-sent");
+    exit;
+
 });
 
 $app->run();
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -8,6 +8,7 @@
 	class User extends Model{
 
 		const SESSION = "User";
+		const SECRET = "HcodePhp7_Secret";
 
 		public static function login($login, $password){
 
@@ -16,14 +17,16 @@
 				":LOGIN"=>$login								
 			));
 
-			if(count($results) === 0){
+			if(count($results) === 0)
+			{
 				throw new \Exception("Invalid user or passsword.");				
 			}
 
 			$data = $results[0];
 
 			// =========== Verify login and password ============= //
-			if (password_verify($password, $data["despassword"]) === true){
+			if (password_verify($password, $data["despassword"]) === true)
+			{
 
 				$user = new User();
 				$user -> setData($data);
@@ -31,7 +34,9 @@
 
 				return $user;
 
-			}else {
+			}
+			else 
+			{
 
 				throw new \Exception("Invalid user or passsword.");
 
@@ -39,7 +44,8 @@
 			// ======================== End ======================= //
 		}
 
-		public static function verifyLogin($inadmin = true){
+		public static function verifyLogin($inadmin = true)
+		{
 
 			if (
 				!isset($_SESSION[User::SESSION])
@@ -49,7 +55,8 @@
 				!(int)$_SESSION[User::SESSION]["iduser"] > 0
 				||
 				(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-			) {
+			) 
+			{
 				
 				header("Location:/admin/login");
 				exit;
@@ -57,16 +64,128 @@
 
 		}
 
-		public static function logout(){
+		public static function logout()
+		{
 
 			$_SESSION[User::SESSION] = NULL;
 
 		}
 
-		public static function listAll(){
+		public static function listAll()
+		{
 
 			$sql = new Sql();
 			return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+		}
+
+		public function save()
+		{
+
+			$sql = new Sql();
+
+			$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",array(
+					":desperson"=>$this->getdesperson(),
+					":deslogin"=>$this->getdeslogin(),
+					":despassword"=>$this->getpassword(),
+					"desemail"=>$this->getdesemail(),
+					":nrphone"=>$this->getnrphone(),
+					":inadmin"=>$this->getinadmin()
+			));
+
+			$this->setData($results[0]);
+
+		}
+
+		public function get($iduser)
+		{
+
+			$sql = new Sql();
+			$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+					":iduser"=>$iduser
+				));
+
+			$this->setData($results[0]);
+
+		}
+
+		public function update()
+		{
+
+			$sql = new Sql();
+			$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
+				array(
+					":iduser"=>$this->getiduser(),
+					":desperson"=>$this->getdesperson(),
+					":deslogin"=>$this->getdeslogin(),
+					":despassword"=>$this->getpassword(),
+					"desemail"=>$this->getdesemail(),
+					":nrphone"=>$this->getnrphone(),
+					":inadmin"=>$this->getinadmin()
+			));
+
+			$this->setData($results[0]);
+
+		}
+
+		public function delete()
+		{
+
+			$sql = new Sql();
+			$sql->query("CALL sp_users_delete(:iduser)", array(
+				":iduser"=>$this->getiduser()
+			));
+
+		}
+
+		public static function getForgot($email)
+		{
+
+			$sql = new Sql();
+			$results = $sql->select("
+				SELECT * FROM tb_persons a
+				INNER JOIN tb_users b USING(idperson)
+				WHERE a.desemail = :email;
+			", array(
+				":email"=>$email
+			));
+
+			if(count($results) === 0)
+			{
+				throw new \Exception("Could not retrieve password.");				
+			}
+			else
+			{
+
+				$data = $results[0];
+				$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+					":iduser"=>$data["iduser"],
+					":desip"=>$_SERVER["REMOTE_ADDR"]
+				));
+
+				if (count($results2) === 0) 
+				{
+					throw new \Exception("Could not retrieve password.");					
+				}
+				else{
+					//Criptografia in PHP.
+					$dataRecovery = $results2[0];
+					$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+					$mailer = new Mailer($data["desemail"], $data["desperson"], "Password reset successfully.", "forgot",
+						array(
+							"name"=>$data["desperson"],
+							"link"=>$link
+					));
+
+					$mailer->send();
+
+					return $data;
+
+				}
+
+			}
 
 		}
 
